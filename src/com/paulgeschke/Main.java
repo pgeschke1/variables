24c853ad9e00d9963491d3297e7c7009dc618b27
pgeschke1 <pgeschke@students.mchenry.edu>
2018-01-27 20:34:09 -0600

    Initial commit

---
+++ src/com/paulgeschke/Main.java
@@ -0,0 +1,26 @@
+package com.paulgeschke;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        byte b = 1;
+        short sh = 2;
+        int i = 33;
+        long l = 44444444L;
+        float f = 5.555555f;
+        double d = 66.6666d;
+        boolean boo = true;
+        char c1 = 't';
+        System.out.println("Byte Value: " + b);
+        System.out.println("Short Value: " + sh);
+        System.out.println("Int Value: " + i);
+        System.out.println("Long Value: " + l);
+        System.out.println("Float Value: " + f);
+        System.out.println("Double Value: " + d);
+        System.out.println("Boolean Value: " + boo);
+        System.out.println("Char Value: " + c1);
+
+
+    }
+}
